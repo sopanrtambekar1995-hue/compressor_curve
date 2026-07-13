dea99e66d47726004707e6d767cd5a1173945efc
sopanrtambekar1995-hue <sopanrtambekar1995@gmail.com>
2026-07-13 08:01:15 +0530

    Add missing unit conversions in HEAD_TO_M

--- compressor_curve_app.py
+++ compressor_curve_app.py
@@ -56,10 +56,10 @@ FLOW_TO_M3HR = {
 
 HEAD_TO_M = {
     'ft': 0.3048, 'feet': 0.3048, 'foot': 0.3048,
-    'ftlbf/lbm': 0.3048, 'lbfft/lbm': 0.3048, 'ftlb/lb': 0.3048,
+    'ftlbf/lbm': 0.3048, 'lbfft/lbm': 0.3048, 'ftlb/lb': 0.3048,'lbft/lb': 0.3048,
     'in': 0.0254, 'inch': 0.0254, 'inches': 0.0254,
     'm': 1.0, 'meter': 1.0, 'metre': 1.0, 'meters': 1.0, 'metres': 1.0,
-    'mm': 0.001,
+    'mm': 0.001,'milimeter': 0.001, 'milimeters': 0.001,
     'kj/kg': 101.9716, 'j/kg': 0.1019716,
     'btu/lb': 237.2075,
 }
